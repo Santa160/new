fb3f84998d468b937289b38a9863404fdda40760
Santa160 <Santa@globizs.com>
2023-12-19 16:33:42 +0530

    udate

--- frontend/views/teacher/index.php
+++ frontend/views/teacher/index.php
@@ -37,6 +37,7 @@ $this->params['breadcrumbs'][] = $this->title;
                 'filter' => [
                     'male' => 'Male',
                     'female' => 'Female',
+                    'other'=>'Other'
                  
                 ],
                 
